e279ef74dd4c5dbf470ad8b29ea6ed2ee649d2dc
VitaliiPozniakov <vertalen89@gmail.com>
2022-08-17 20:38:56 +0300

    17.08

--- index.js
+++ index.js
@@ -27,6 +27,8 @@
 
 // console.log(arrayDiff([15,-19,-2],[15,-19,-2]));
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
 
 // For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
@@ -71,7 +73,9 @@
 //     .join("");
 // }
 
-// console.log(duplicateEncode("(( @")) 
+// console.log(duplicateEncode("(( @"))
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
 
@@ -85,9 +89,6 @@
 // }
 
 // console.log(isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]));
-
-
-
 
 // You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 
@@ -103,7 +104,7 @@
 
 // function getMiddle(s)
 // {
-//   const array = s.split('') 
+//   const array = s.split('')
 // if (array.length % 2 === 0){
 // return array.splice(array.indexOf(array[array.length/2 - 1]) , 2).join('')
 // }
@@ -118,15 +119,127 @@
 // }
 
 // function getMiddle(s){
-   
+
 //   let evenOdd = s.length % 2 === 0
 //   // console.log(evenOdd)
 //   let minMid = s.charAt(s.length/2-1)
 //   // console.log(minMid)
 //   let mid = s.charAt(s.length/2)
 //   // console.log(mid)
-  
+
 //   return (evenOdd == true)? minMid.concat(mid): mid
 // }
 
 // console.log(getMiddle("testing"))
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+// let result = 0;
+// function persistence(num) {
+//   result += 1;
+//   if (num < 10) {
+//     return 0;
+//   }
+
+//   const array = String(num).split("");
+
+//   let mult = 1;
+//   for (const n of array) {
+//     mult *= Number(n);
+//   }
+
+//   while (mult > 9) {
+//     persistence(mult);
+
+//     break;
+//   }
+
+//   return result;
+// }
+
+// function persistence(num) {
+//     let times = 0;
+
+//     num = num.toString();
+
+//     while (num.length > 1) {
+//       times += 1;
+//       num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+//     }
+
+//     return times;
+//  }
+
+// console.log(persistence(999));
+
+// console.log([2, 5].reduce((a, b) => a * b))
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+// function solution(number){
+//     if (number < 0) {
+//         return 0
+//     }
+//     let result = 0
+//     for (let i = 1; i < number; i += 1){
+//         // console.log(i)
+//         if (i % 3 === 0 || i % 5 === 0 ) {
+//             console.log(i)
+//             result += i
+//         }
+//     }
+//  return result
+// }
+
+// console.log(solution(10))
+
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// function solution(string) {
+//     let upperCaseIndexes = [];
+//     let result = "";
+
+//   const strToLowerCase = string.toLowerCase();
+
+//   for (let i = 0; i < string.length; i += 1) {
+//     if (strToLowerCase[i] !== string[i]) {
+//       upperCaseIndexes.push(i);
+//     }
+
+//     const array = string.split("");
+
+//     for (let i = 0; i < upperCaseIndexes.length; i += 1) {
+//       array.splice(upperCaseIndexes[i] + i, 0, " ");
+//     }
+//     result = array.join("");
+//   }
+//   return result;
+// }
+
+// const solution = string => string.replace(/[A-Z]/g, ' $&');
+
+// function solution(string) {
+//     string = string.split('').map(el => {
+//       if (el === el.toUpperCase()) {
+//         el = ' ' + el
+//       }
+//       return el
+//     })
+//     return string.join('')
+//   }
+
+// console.log(solution("camelCasingTest"));
--- typescript/generic.ts
+++ typescript/generic.ts
@@ -8,3 +8,65 @@ return array.reverse( )
 
 reverse(arrayOfNumbers)
 reverse(arrayOfStrings)
+
+function mergeObject <T extends object, R extends object > (a: T, b: R): T & R {
+    return Object.assign({}, a, b)
+}
+
+const merged = mergeObject({name: 'Vitalii'}, {age: 32})
+
+
+interface ILength {
+     length: number
+}
+
+function withCount<T extends ILength>(value: T): {value: T, count: string}{
+    return {
+        value,
+        count: `В этом обьекте ${value.length } символов`
+    }
+}
+
+// console.log(withCount('Hello'))
+// console.log(withCount(['Hello', 'World']))
+// console.log(withCount(20))
+// console.log(withCount({length: 20}))
+
+function getObjectValue<T extends object, R extends keyof T>(obj: T, key: R) {
+    return obj[key]
+}
+
+const person = {
+    age: 32,
+    name: 'Vitalii'
+} 
+console.log(getObjectValue(person, 'age'))
+console.log(getObjectValue(person, 'name'))
+console.log(getObjectValue(person, 'job'))
+
+class Collection<T extends number | string | boolean> {
+    
+
+    constructor (private _items: T[] = [ ]){}
+
+    add(item: T){
+        this._items.push(item)
+    }
+
+    remove(item: T){
+        this._items = this._items.filter(e =>  e !== item)
+    }
+
+    get items() :T[] {
+         return this.items
+    }
+}
+
+const strings = new Collection (['i', 'am'])
+strings.add('!')
+strings.remove('am') 
+
+
+const numbers = new Collection ([1, 2, 3])
+numbers.add(4)
+numbers .remove(3) 
